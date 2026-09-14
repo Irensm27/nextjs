b19188498143c3bde4a28c2ff54761951cd4c256
irensm27 <semenets.irina27@gmail.com>
2026-09-14 18:43:33 +0300

    Homework 1 start

---
+++ src/app/(public)/comments/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "",
+};
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const  = ({ children }: Props) => {
+    return (
+        <>
+            
+            {children}
+        </>
+    );
+};
+
